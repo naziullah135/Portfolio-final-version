be1a8f486bc7f6f4bdf1928765d0e89b2e11996c
Naziullah Shawn <naziullahshawn135@gmail.com>
2024-04-28 00:11:49 +0600

    remove some unnessary code

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,9 +4,6 @@ import { Analytics } from "@vercel/analytics/react";
 import MasterLayout from "@/components/Common/MasterLayout";
 import Hotjar from '@hotjar/browser';
 
-import Script from "next/script";
-import hotjarScript from "../../public/hotjar";
-
 const siteId = 4961548;
 const hotjarVersion = 6;
 
@@ -30,11 +27,6 @@ export default function RootLayout({ children }) {
       <ThemeRegistry>
         <body className={inter.className}>
           {" "}
-          <Script
-            id="hotjar-script"
-            strategy="afterInteractive"
-            onLoad={hotjarScript}
-          />
           <MasterLayout>{children}</MasterLayout>
           <Analytics />
         </body>
